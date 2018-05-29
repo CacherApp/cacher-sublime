5810a886596a5bdb886dbc2a403631cdc29b6096
Rui Jiang <rjiang.bb@gmail.com>
2018-05-29 12:17:44 -0400

    Started on create snippet command.

--- cacher_create_snippet.py
+++ cacher_create_snippet.py
@@ -1,9 +1,26 @@
 import sublime_plugin
+import sublime
 
 
-class CacherCreateSnippetCommand(sublime_plugin.WindowCommand):
-    def run(self):
-        self.window.active_view().show_popup_menu(
-            ["foobar", "two"],
-            lambda index: print(index)
-        )
+class SnippetLibraryInputHandler(sublime_plugin.ListInputHandler):
+    @staticmethod
+    def list_items():
+        return [
+            ("Persona", "Team 1")
+        ]
+
+
+class CacherCreateSnippetCommand(sublime_plugin.TextCommand):
+    @staticmethod
+    def run(edit, files, group, index, snippet_library):
+        print(group)
+        print(index)
+
+        # Creating snippet from tab context
+        if group >= 0 and index >= 0:
+            view = sublime.active_window().sheets_in_group(group)[index].view()
+            body = view.substr(sublime.Region(0, view.size()))
+
+    @staticmethod
+    def input(args):
+        return SnippetLibraryInputHandler()
