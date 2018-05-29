a6ade3d6abc986467e18250fb43adba84f8c5e2b
Rui Jiang <rjiang.bb@gmail.com>
2018-05-29 18:28:44 -0400

    Fix issue with input not showing up.

--- cacher_create_snippet.py
+++ cacher_create_snippet.py
@@ -10,14 +10,14 @@ class SnippetLibraryInputHandler(sublime_plugin.ListInputHandler):
         ]
 
 
-class CacherCreateSnippetCommand(sublime_plugin.TextCommand):
+class CacherCreateSnippetCommand(sublime_plugin.WindowCommand):
     @staticmethod
-    def run(edit, files, group, index, snippet_library):
-        print(group)
-        print(index)
+    def run(snippet_library, **args):
+        group = args["group"]
+        index = args["index"]
 
         # Creating snippet from tab context
-        if group >= 0 and index >= 0:
+        if (group and group >= 0) and (index and index >= 0):
             view = sublime.active_window().sheets_in_group(group)[index].view()
             body = view.substr(sublime.Region(0, view.size()))
 
